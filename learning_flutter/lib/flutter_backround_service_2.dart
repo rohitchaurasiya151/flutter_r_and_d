6ade0b5cf78db4e4ae7fdf79a67448ac884fd00b
rohit <rohitchaursiya7383@gmail.com>
2022-02-28 15:36:53 +0530

    bg

---
+++ learning_flutter/lib/flutter_backround_service_2.dart
@@ -0,0 +1,181 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeService();
+  runApp(const MyApp());
+}
+
+Future<void> initializeService() async {
+  final service = FlutterBackgroundService();
+  await service.configure(
+    androidConfiguration: AndroidConfiguration(
+      // this will executed when app is in foreground or background in separated isolate
+      onStart: onStart,
+
+      // auto start service
+      autoStart: true,
+      isForegroundMode: true,
+    ),
+    iosConfiguration: IosConfiguration(
+      // auto start service
+      autoStart: true,
+
+      // this will executed when app is in foreground in separated isolate
+      onForeground: onStart,
+
+      // you have to enable background fetch capability on xcode project
+      onBackground: onIosBackground,
+    ),
+  );
+}
+
+// to ensure this executed
+// run app from xcode, then from xcode menu, select Simulate Background Fetch
+void onIosBackground() {
+  WidgetsFlutterBinding.ensureInitialized();
+  print('FLUTTER BACKGROUND FETCH');
+}
+
+void onStart() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final service = FlutterBackgroundService();
+  service.onDataReceived.listen((event) {
+    if (event!["action"] == "setAsForeground") {
+      service.setForegroundMode(true);
+      return;
+    }
+
+    if (event["action"] == "setAsBackground") {
+      service.setForegroundMode(false);
+    }
+
+    if (event["action"] == "stopService") {
+      service.stopBackgroundService();
+    }
+  });
+
+  // bring to foreground
+  service.setForegroundMode(true);
+  Timer.periodic(const Duration(seconds: 1), (timer) async {
+    if (!(await service.isServiceRunning())) timer.cancel();
+    service.setNotificationInfo(
+      title: "My App Service",
+      content: "Updated at ${DateTime.now()}",
+    );
+
+    // test using external plugin
+    final deviceInfo = DeviceInfoPlugin();
+    String? device;
+    if (Platform.isAndroid) {
+      final androidInfo = await deviceInfo.androidInfo;
+      device = androidInfo.model;
+    }
+
+    if (Platform.isIOS) {
+      final iosInfo = await deviceInfo.iosInfo;
+      device = iosInfo.model;
+    }
+
+    service.sendData(
+      {
+        "current_date": DateTime.now().toIso8601String(),
+        "device": device,
+      },
+    );
+  });
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String text = "Stop Service";
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Service App'),
+        ),
+        body: Column(
+          children: [
+            StreamBuilder<Map<String, dynamic>?>(
+              stream: FlutterBackgroundService().onDataReceived,
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                final data = snapshot.data!;
+                String? device = data["device"];
+                DateTime? date = DateTime.tryParse(data["current_date"]);
+                return Column(
+                  children: [
+                    Text(device ?? 'Unknown'),
+                    Text(date.toString()),
+                  ],
+                );
+              },
+            ),
+            ElevatedButton(
+              child: const Text("Foreground Mode"),
+              onPressed: () {
+                FlutterBackgroundService()
+                    .sendData({"action": "setAsForeground"});
+              },
+            ),
+            ElevatedButton(
+              child: const Text("Background Mode"),
+              onPressed: () {
+                FlutterBackgroundService()
+                    .sendData({"action": "setAsBackground"});
+              },
+            ),
+            ElevatedButton(
+              child: Text(text),
+              onPressed: () async {
+                final service = FlutterBackgroundService();
+                var isRunning = await service.isServiceRunning();
+                if (isRunning) {
+                  service.sendData(
+                    {"action": "stopService"},
+                  );
+                } else {
+                  service.start();
+                }
+
+                if (!isRunning) {
+                  text = 'Stop Service';
+                } else {
+                  text = 'Start Service';
+                }
+                setState(() {});
+              },
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            FlutterBackgroundService().sendData({
+              "hello": "world",
+            });
+          },
+          child: const Icon(Icons.play_arrow),
+        ),
+      ),
+    );
+  }
+}
--- learning_flutter/lib/main.dart
+++ learning_flutter/lib/main.dart
@@ -1,1 +1,449 @@
-dsfdhgfcj
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case HomePage.route:
+            return MaterialPageRoute(
+                builder: (context) => Scaffold(
+                      body: Container(
+                        color: Colors.amber,
+                        child: const Text("home"),
+                      ),
+                    ),
+                settings: const RouteSettings(name: HomePage.route));
+        }
+      },
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  static const String route = "/home";
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _pageIndex = 0;
+  int _drawerIndex = 0;
+  List<int> screenStack = [0];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("page $_pageIndex"),
+      ),
+      body: SafeArea(
+        child: WillPopScope(
+          onWillPop: () async {
+            if (screenStack.length > 1) {
+              setState(() {
+                screenStack.removeLast();
+                _pageIndex = screenStack[screenStack.length - 1];
+              });
+              return false;
+            }
+            return true;
+          },
+          child: IndexedStack(
+            index: (_drawerIndex < _pageIndex) ? _pageIndex : _drawerIndex,
+            children: <Widget>[
+              Container(
+                color: Colors.amber,
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const Scaffold(
+                                        body: Center(
+                                          child: Text("DetailsPage"),
+                                        ),
+                                      )));
+                        },
+                        child: const Text("navigate"),
+                      ),
+                      const Text('Home'),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                color: Colors.green,
+                child: const Center(child: Text('Business')),
+              ),
+              Container(
+                color: Colors.amber,
+                child: const Center(child: Text('Technology')),
+              ),
+              // Container(
+              //   color: Colors.blueAccent,
+              //   child: const Center(child: Text('Education')),
+              // ),
+              const AttendanceScreen(),
+              Container(
+                color: Colors.deepOrange,
+                child: const Center(child: Text('Others')),
+              ),
+            ],
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            children: [
+              ListTile(
+                onTap: () {
+                  Navigator.of(context).pop();
+                  setState(
+                    () {
+                      _pageIndex = 0;
+                      _drawerIndex = _pageIndex;
+                      if (_pageIndex == 0) {
+                        screenStack = [0];
+                      } else if (!screenStack.contains(_pageIndex)) {
+                        screenStack.add(_pageIndex);
+                      }
+                    },
+                  );
+                },
+                title: const Text("Home"),
+              ),
+              ListTile(
+                onTap: () {
+                  Navigator.of(context).pop();
+                  setState(
+                    () {
+                      _pageIndex = 1;
+                      _drawerIndex = _pageIndex;
+                      if (_pageIndex == 0) {
+                        screenStack = [0];
+                      } else if (!screenStack.contains(_pageIndex)) {
+                        screenStack.add(_pageIndex);
+                      }
+                    },
+                  );
+                },
+                title: const Text("Business"),
+              ),
+              ListTile(
+                onTap: () {
+                  Navigator.of(context).pop();
+                  setState(
+                    () {
+                      _pageIndex = 2;
+                      _drawerIndex = _pageIndex;
+                      if (_pageIndex == 0) {
+                        screenStack = [0];
+                      } else if (!screenStack.contains(_pageIndex)) {
+                        screenStack.add(_pageIndex);
+                      }
+                    },
+                  );
+                },
+                title: const Text("Technology"),
+              ),
+              ListTile(
+                onTap: () {
+                  Navigator.of(context).pop();
+                  setState(
+                    () {
+                      _pageIndex = 3;
+                      _drawerIndex = _pageIndex;
+                      if (_pageIndex == 0) {
+                        screenStack = [0];
+                      } else if (!screenStack.contains(_pageIndex)) {
+                        screenStack.add(_pageIndex);
+                      }
+                    },
+                  );
+                },
+                title: const Text("Education"),
+              ),
+              ListTile(
+                onTap: () {
+                  Navigator.of(context).pop();
+                  setState(
+                    () {
+                      _drawerIndex = 4;
+                    },
+                  );
+                },
+                title: const Text("Others"),
+              )
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.computer),
+            label: 'Technology',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Education',
+          ),
+        ],
+        currentIndex: _pageIndex,
+        onTap: (int index) {
+          setState(
+            () {
+              _pageIndex = index;
+              _drawerIndex = _pageIndex;
+              if (_pageIndex == 0) {
+                screenStack = [0];
+              } else if (!screenStack.contains(_pageIndex)) {
+                screenStack.add(_pageIndex);
+              }
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class DetailRoute extends StatelessWidget {
+  late TextEditingController? textEditingController;
+  int? index;
+
+  DetailRoute({Key? key, this.textEditingController, this.index})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class AttendanceScreen extends StatefulWidget {
+  const AttendanceScreen({Key? key}) : super(key: key);
+
+  @override
+  _AttendanceScreenState createState() => _AttendanceScreenState();
+}
+
+class _AttendanceScreenState extends State<AttendanceScreen> {
+  static var countdownDuration = const Duration(minutes: 10);
+  static var countdownDuration1 = const Duration(minutes: 10);
+  Duration duration = const Duration();
+  Duration duration1 = const Duration();
+  Timer? timer;
+  Timer? timer1;
+  bool countDown = true;
+  bool countDown1 = true;
+
+  @override
+  void initState() {
+    
+    var hours;
+    var mints;
+    var secs;
+    hours = int.parse("00");
+    mints = int.parse("00");
+    secs = int.parse("00");
+    countdownDuration = Duration(hours: hours, minutes: mints, seconds: secs);
+    startTimer();
+    reset();
+    var hours1;
+    var mints1;
+    var secs1;
+    hours1 = int.parse("10");
+    mints1 = int.parse("00");
+    secs1 = int.parse("00");
+    countdownDuration1 =
+        Duration(hours: hours1, minutes: mints1, seconds: secs1);
+    startTimer1();
+    reset1();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        body: Container(
+          color: Colors.black12,
+          width: double.infinity,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  "Timer",
+                  style: TextStyle(fontSize: 25),
+                ),
+                Container(
+                    margin:const EdgeInsets.only(top: 30, bottom: 30),
+                    child: buildTime()),
+                const SizedBox(
+                  height: 20,
+                ),
+               const Text(
+                  "Count down timer",
+                  style: TextStyle(fontSize: 25),
+                ),
+                Container(
+                    margin:const EdgeInsets.only(top: 30, bottom: 30),
+                    child: buildTime1()),
+              ]),
+        ),
+      ),
+    );
+  }
+
+  Future<bool> _onWillPop() async {
+    final isRunning = timer == null ? false : timer!.isActive;
+    if (isRunning) {
+      timer!.cancel();
+    }
+    Navigator.of(context, rootNavigator: true).pop(context);
+    return true;
+  }
+
+  void reset() {
+    if (countDown) {
+      setState(() => duration = countdownDuration);
+    } else {
+      setState(() => duration = Duration());
+    }
+  }
+
+  void reset1() {
+    if (countDown) {
+      setState(() => duration1 = countdownDuration1);
+    } else {
+      setState(() => duration1 = const Duration());
+    }
+  }
+
+  void startTimer() {
+    timer = Timer.periodic(const Duration(seconds: 1), (_) => addTime());
+  }
+
+  void startTimer1() {
+    timer = Timer.periodic(const Duration(seconds: 1), (_) => addTime1());
+  }
+
+  void addTime() {
+    const addSeconds = 1;
+    setState(() {
+      final seconds = duration.inSeconds + addSeconds;
+      if (seconds < 0) {
+        timer?.cancel();
+      } else {
+        duration = Duration(seconds: seconds);
+      }
+    });
+  }
+
+  void addTime1() {
+    const addSeconds = 1;
+    setState(() {
+      final seconds = duration1.inSeconds - addSeconds;
+      if (seconds < 0) {
+        timer1?.cancel();
+      } else {
+        duration1 = Duration(seconds: seconds);
+      }
+    });
+  }
+
+  Widget buildTime() {
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    final hours = twoDigits(duration.inHours);
+    final minutes = twoDigits(duration.inMinutes.remainder(60));
+    final seconds = twoDigits(duration.inSeconds.remainder(60));
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      buildTimeCard(time: hours, header: 'HOURS'),
+      const SizedBox(
+        width: 8,
+      ),
+      buildTimeCard(time: minutes, header: 'MINUTES'),
+      const SizedBox(
+        width: 8,
+      ),
+      buildTimeCard(time: seconds, header: 'SECONDS'),
+    ]);
+  }
+
+  Widget buildTime1() {
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    final hours = twoDigits(duration1.inHours);
+    final minutes = twoDigits(duration1.inMinutes.remainder(60));
+    final seconds = twoDigits(duration1.inSeconds.remainder(60));
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      buildTimeCard(time: hours, header: 'HOURS'),
+      const SizedBox(
+        width: 8,
+      ),
+      buildTimeCard(time: minutes, header: 'MINUTES'),
+      const SizedBox(
+        width: 8,
+      ),
+      buildTimeCard(time: seconds, header: 'SECONDS'),
+    ]);
+  }
+
+  Widget buildTimeCard({required String time, required String header}) =>
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(20)),
+            child: Text(
+              time,
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  fontSize: 50),
+            ),
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          Text(header, style: const TextStyle(color: Colors.black45)),
+        ],
+      );
+}
